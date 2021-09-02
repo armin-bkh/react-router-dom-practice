fde943657733e325c6d2a071f150545de5058e7c
armin bakhshi <arminbkh0921@yahoo.com>
2021-09-02 14:45:42 +0430

    create homepage and aboutuspage component and add reset css

---
+++ src/Pages/AboutUsPage.jsx
@@ -0,0 +1,9 @@
+const AboutUsPage = () => {
+    return ( 
+        <section>
+            <h1>about us page</h1>
+        </section>
+     );
+}
+ 
+export default AboutUsPage;
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return ( 
+        <section>
+            <h1>home page</h1>
+        </section>
+     );
+}
+ 
+export default HomePage;
